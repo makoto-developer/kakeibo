dc25171e378015c879bbb2f83aec50b359bb0afd
makoto-engineer <72484465+makoto-engineer@users.noreply.github.com>
2022-03-27 22:43:51 +0900

    wip

---
+++ frontend/src/components/PageAddBlance/index.tsx
@@ -0,0 +1,176 @@
+import "react-datepicker/dist/react-datepicker.css";
+import s from './style.module.scss'
+
+import {useForm} from "react-hook-form";
+import {DatePicker} from "../DatePicker";
+import {useState} from "react";
+import {useRouter} from "next/router";
+import Button from "../Button";
+import ApiClient from "../../util/api";
+
+const Add = () => {
+  const defaultValues = {
+    amount: 0,
+    datetime: null,
+    method: null,
+    test: null,
+    sss: null
+  }
+  const router = useRouter()
+
+  type Inputs = {
+    expense: number;
+    date: string;
+    method: string
+  };
+  const {register, control, handleSubmit, watch, formState: {errors}} = useForm();
+  console.log('register:', register)
+  const [check, setCheck] = useState('現金')
+
+  const putBalance = async () => {
+    // field :day, :date
+    // field :expense, :integer
+    // field :income, :integer
+    // field :method, :string
+    // field :title, :string
+    // field :who, :string
+
+    const postData = {
+      day: '',
+      expense: '',
+      income: '',
+      method: '',
+      title: '',
+      who: '',
+
+    }
+    return await ApiClient.post("/api/balance/", postData)
+      .then(res => res.data)
+  }
+
+  const onSubmit = (data: any) => console.log(data);
+  const payMethods = ['現金', 'd-card', 'jal-card']
+
+  const errorMessage = () => {
+    if (errors?.amount?.type !== "required") return <></>
+    let message = ''
+    switch (errors.amount.type) {
+      case 'min':
+        message = '0円以上を入力してください'
+        break
+      case 'max':
+        message = '5,000,000円未満を入力してください'
+        break
+      case 'pattern':
+        message = '数値を入力してください'
+        break
+    }
+    return <span style={{'color': 'red'}}>{message}</span>
+  }
+
+  return <div>
+    <div className={s.root}>
+      <Button name={'戻る'} onClick={() => router.back()} />
+      <div style={{
+        'display': 'flex',
+        'justifyContent': 'center',
+        'alignItems': 'center',
+        'minHeight': '100vh',
+        'textAlign': 'center'
+      }}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label>金額</label>
+          <div style={{
+            'textAlign': 'center'
+          }}>
+            <input
+              defaultValue='0'
+              {...register("expense", {min: 0, max: 5_000_000, required: true, pattern: /^[\d]+$/i})}
+              style={{
+                'height': '40px',
+                'width': '50%',
+                'fontSize': '20px'
+              }}
+            />
+            <div>
+              {errorMessage()}
+            </div>
+          </div>
+          <label style={{
+            'display': 'inlineBlock',
+            'marginTop': '20px'
+          }}>支払い方法</label>
+          <div
+            style={{
+              'display': 'flex',
+              'justifyContent': 'spaceEvenly',
+              'alignItems': 'center',
+              'border': 'none'
+            }}>
+            <input
+              {...register("method")}
+              id='item-1'
+              className='radio-inline__input'
+              type='radio'
+              name='accessible-radio'
+              value='現金'
+              checked={check === '現金'}
+            />
+
+            <label className='radio-inline__label' onClick={() => setCheck('現金')}>
+              現金
+            </label>
+            <input
+              {...register("method")}
+              id='item-2'
+              className='radio-inline__input'
+              type='radio'
+              name='accessible-radio'
+              value='d-card'
+              checked={check === 'd-card'}
+            />
+            <label className='radio-inline__label' onClick={() => setCheck('d-card')}>
+              d-card
+            </label>
+            <input
+              {...register("method")}
+              id='item-3'
+              className='radio-inline__input'
+              type='radio'
+              name='accessible-radio'
+              value='jal-card'
+              checked={check === 'jal-card'}
+            />
+            <label className='radio-inline__label' onClick={() => setCheck('jal-card')}>
+              jal-card
+            </label>
+            {errors?.method?.type && errors.method.type === "required" &&
+            <span style={{'color': 'red'}}>必須です</span>}
+          </div>
+          <div>
+            <DatePicker
+              {...register("date")}
+              label='datetime'
+              name='datetime'
+              control={control}
+              error={errors?.datetime?.message}
+            />
+          </div>
+          <input
+            type='submit'
+            style={{
+              'backgroundColor': '#66CCFF',
+              'border': 'none',
+              'width': '90vw',
+              'height': '30px',
+              'flexShrink': '0',
+              'marginTop': '10px'
+            }}
+          />
+        </form>
+      </div>
+    </div>
+  </div>
+}
+
+export default Add
---
+++ frontend/src/components/PageBalance/index.tsx
@@ -0,0 +1,92 @@
+import dayjs from 'dayjs';
+import ApiClient from "../../util/api";
+import {useEffect, useState} from "react";
+import Index from "../Button";
+import {useRouter} from "next/router";
+import s from './style.module.scss'
+
+type BalanceType = {
+  day: string,
+  expense: number,
+  id: number,
+  income: number,
+  title: string,
+  method: string,
+  who: string,
+}
+
+const Balance = () => {
+  const router = useRouter()
+  const fetchBalance = async () => {
+    return await ApiClient.get("/api/balance/")
+      .then(res => res.data)
+  }
+  // response example
+  // const response = [
+  //     {day: "2022-01-25", expense: 2000, id: 3, income: 0, title: "昼ごはん", who: "alice"},
+  //     {day: "2022-02-25", expense: 7000, id: 1, income: 0, title: "ガソリン", who: "tom"},
+  //     {day: "2022-01-25", expense: 0, id: 2, income: 250000, title: "振り込み", who: "tom"}
+  // ]
+
+  useEffect(() => {
+    fetchBalance().then(res => setBalanceData(res.data)
+    )
+  }, [])
+  const [balanceData, setBalanceData] = useState<Array<BalanceType>>([])
+  const shapingData = (list: Array<BalanceType>) => list.sort((a: BalanceType, b: BalanceType) => {
+    if (dayjs(a.day).isAfter(b.day)) {
+      return 1
+    } else {
+      return -1
+    }
+  }).reduce((acc: Array<
+  BalanceType & { balance: number }>, val: BalanceType) => {
+    if (acc.length === 0) {
+      acc.push({...val, balance: (val.income - val.expense)})
+      return acc
+    }
+    acc.push({...val, balance: (val.income - val.expense + acc[acc.length - 1].balance)})
+    return acc
+  }, [])
+    .reverse()
+
+  console.log("balanceData:", balanceData)
+  const data = shapingData(balanceData)
+  return (
+    <div>
+      <Index name={'追加'} type={'primary'} onClick={() => router.push('/add')} />
+      <table>
+        <thead>
+          <tr>
+            <th>日付</th>
+            <th>入金/出費</th>
+            <th>残高</th>
+            <th>人</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map(balance => {
+              return <tr key={balance.id}>
+                <td>{dayjs(balance.day).format('M/D')}</td>
+                {
+                  balance.expense === 0 &&
+                  <td style={{'color': '#009966'}}>{balance.income.toLocaleString()}</td>
+                }
+                {
+                  balance.income === 0 &&
+                  <td style={{'color': '#FF3333'}}>-{balance.expense.toLocaleString()}</td>
+                }
+                <td>{balance.balance.toLocaleString()}</td>
+                <td>{balance.title}/{balance.method}</td>
+                <td>{balance.who}</td>
+              </tr>
+            })
+          }
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Balance
