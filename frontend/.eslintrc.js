9a357286669ce823571337a204e5086194080580
makoto-developer <72484465+makoto-developer@users.noreply.github.com>
2023-11-20 00:09:37 +0900

    updatee

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,4 +1,5 @@
 /** @type {import('eslint').Linter.Config} */
+const [off, warn, error] = ["off", "warn", "error"]
 module.exports = {
   extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
 };
